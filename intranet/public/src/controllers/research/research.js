3fc6c4a246713a024b177fff858dd4750d5e380a
Camilo Campo <mcp.ccampo@gmail.com>
2021-01-10 14:09:31 -0500

    added react components

---
+++ intranet/public/src/controllers/research/research.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class research extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: [] };
+  }
+  render() {
+    return (
+      <div>
+        <h1>Research Component</h1>
+      </div>
+    );
+  }
+}
+
+export default research;
